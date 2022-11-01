5d67e8492debb9d36e824aa51e11c83da2c86eaf
Philippe Martinet <phi.mar.email@gmail.com>
2022-11-01 13:55:44 +0100

    Test push to github

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+   int a[10]= {1,2,3,4,5,6,7,8,9,10};
+   char b[10]= {'a', 'A', 'b', 'B', 'c', 'C',
+      'd', 'D', 'e', 'E'};
+
+   printf("The third element of a is: %d\n",a[2]);
+   printf("The last element of b is: %c\n",b[9]);
+
+   
+   return 0;
+}
+
