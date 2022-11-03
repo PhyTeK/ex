6da4d0e50bc9d06fe801d15b39be44efe6d75e12
Philippe Martinet <13866156+PhyTeK@users.noreply.github.com>
2022-11-03 10:49:57 +0100

    fibonacci exercise

---
+++ fibonacciQ.c
@@ -0,0 +1,34 @@
+/*                                                                              
+ Exercise:                                                                      
+ Replace ??? with something useful!                                             
+                                                                                
+*/
+
+#include<stdio.h>
+?????
+
+int f(int);
+
+int main(????)
+{
+  int n, i = 0, c;
+  // Test if there is one and only one argument after program's name.           
+  // Return 1 if not.                                                           
+
+  if(??){
+
+  }
+
+  // Transform a string into an int                                             
+
+  n=??
+
+  printf("Fibonacci series terms are:\n");
+
+  for (c = 1; c <= n; c++){
+    printf("%d\n", f(i));
+    i++;
+  }
+
+  return 0;
+}
