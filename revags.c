c811bdb83ad0a650728a543602774d794f19e7c8
Philippe Martinet <13866156+PhyTeK@users.noreply.github.com>
2022-11-03 10:40:10 +0100

    Add files via upload

---
+++ revags.c
@@ -0,0 +1,15 @@
+/*                                                                              
+   Exercise: Write a C-code revarg that invert its online arguments.            
+                                                                                
+   E.g.                                                                         
+                                                                                
+   INPUT: ./revarg file1 file2 file3                                            
+   OUTPUT: file3 file2 file1                                                    
+                                                                                
+   For local users:                                                             
+   E.g If you have to send code.c code.h and makefile:                          
+                                                                                
+   scp code.c code.h makefile username@77.53.56.197:Exam1/                      
+                                                                                
+   replace username with your 6 characters username on rasp.                    
+*/
