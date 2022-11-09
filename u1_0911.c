383c55072a759e7ac8114b346abeba922d2413f2
Philippe Martinet <13866156+PhyTeK@users.noreply.github.com>
2022-11-09 14:15:06 +0100

    malloc calloc exercise

---
+++ u1_0911.c
@@ -0,0 +1,16 @@
+/*
+  Exercise: Write a program called u1 that get two row arguments that
+  are the numbers of rows anc columns of 2D array.
+  
+  1) Allocate dynamically memory to an array of int called arr.
+  2) Initialize each element with the value of the sum of the 
+     specific row with the specific column. (i+j)
+  3) Print the resulting matrix on the screen (stdout).
+  4) Print the resulting matrix to a file called file1
+  5) Declare a function init.
+  6) Define the function init that initialize arr with the elements (i+j)
+
+*/
+
+
+
